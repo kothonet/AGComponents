1ebf31d8ec5b813f36095f62b5299c660913355e
kothonet <kotho.net@gmail.com>
2016-07-08 14:27:11 -0300

    Add files via upload

---
+++ AGMultiButton/AGMultiButton.js
@@ -0,0 +1,73 @@
+var AGMultiButton = InputBaseComponent.extend({
+
+	draw: function(myData) {
+		var mySelf = this;
+		var param = mySelf._getParameterValue();
+
+		var myDiv = $("<div/>").attr({
+		  "data-toggle": "buttons"
+	  	}).addClass(mySelf.orientation);
+
+		myDiv.on("click", "label", (function(_myDiv, _mySelf) {
+		  	return function() {
+				if($(this).find("input").attr("type") === "radio") {
+					if ($(this).hasClass("active")) return;
+
+					Dashboards.fireChange(_mySelf.parameter, $(this).find("input").val());
+				} else {
+					var inputs = $(_myDiv).find("label").find("input");
+					var params = "";
+					for (var i=0; i<inputs.length; i++) {
+
+						var active = $(inputs[i]).parent().hasClass("active");
+						if ($(inputs[i]).parent().attr("id") === $(this).attr("id")) {
+							active = !active;
+						}
+
+						if (active) {
+							params += params !== "" ? "," : "";
+							params += $(inputs[i]).val();
+						}
+					}
+
+					Dashboards.fireChange(_mySelf.parameter, params);
+				}
+			}
+		})(myDiv, mySelf));
+
+		if (mySelf.size !== 'empty') {
+		  	myDiv.addClass(mySelf.size);
+		}
+
+		for (var i=0; i<myData.length; i++) {
+
+			var id = mySelf.valueAsId ? myData[i][1] : myData[i][0];
+			var text = myData[i][1];
+
+			var myLabel = $("<label/>").attr({
+			        id: "btn_" + id,
+			        class: "btn"
+			    }).addClass(mySelf.style).text(text);
+
+			var params = param != null ? param.split(",") : [];
+			if (params.indexOf(id) > -1) {
+			    $(myLabel).addClass("active");
+			}
+
+			var myInput = $("<input/>").attr({
+			  id: "rad_" + id,
+			  type: mySelf.buttonType,
+			  value: id,
+			  autocomplete: "off",
+			  name: "options"
+			});
+
+			myLabel.append(myInput);
+			myDiv.append(myLabel);
+		}
+
+		$("#" + this.htmlObject).empty();
+		$("#" + this.htmlObject).append(myDiv);
+	}
+
+});
---
+++ AGSelect/AGSelect.js
@@ -0,0 +1,59 @@
+var AGSelect = InputBaseComponent.extend({
+
+	draw: function(myData) {
+		var mySelf = this;
+		var param = mySelf._getParameterValue();
+
+		var mySelect = $("<select/>").attr({
+        	class: "selectpicker show-tick show-menu-arrow",
+        	"data-width": "fit",
+        	id: "sel_" + this.htmlObject,
+        	"data-size": "5",
+    	});
+
+		mySelect.on("change", (function(_mySelf) {
+			return function() {
+				var value = $(this).val() !== null ? $(this).val().toString() : "";
+
+				if (value === "" && _mySelf.valueIfEmpty !== "") {
+					value = _mySelf.valueIfEmpty;
+				}
+
+		        Dashboards.fireChange(_mySelf.parameter, value);
+			}
+	    })(mySelf));
+
+		if (mySelf.multiple) {
+	        mySelect.attr("multiple", "");
+	    }
+
+	    if (mySelf.search) {
+	        mySelect.attr("data-live-search","true");
+	    }
+
+		if (mySelf.title !== "") {
+			mySelect.attr("title", mySelf.title);
+		}
+
+		for (var i=0; i < myData.length; i++) {
+
+	        var id = mySelf.valueAsId ? myData[i][1] : myData[i][0];
+	        var text = myData[i][1];
+
+	        var myOption = $("<option/>").attr("value", id).text(text);
+
+			var params = param !== null ? param.split(",") : [];
+	        if (params.indexOf(id) > -1) {
+	            myOption.attr("selected", "");
+	        }
+
+	        mySelect.append(myOption);
+	    }
+
+		$("#" + this.htmlObject).empty();
+		$("#" + this.htmlObject).append(mySelect);
+
+		mySelect.selectpicker('render');
+	}
+
+});
